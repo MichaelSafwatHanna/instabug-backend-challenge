57053ee0becfec9ec34dbdbea862cfdacd85d53e
Michael Safwat <michaelsafwat.hanna@gmail.com>
2022-05-05 12:59:04 +0200

    feat: improve docker entrypoint

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,2 @@
 Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_HOST'])
 
-# Need to be decoupled.
-Message.__elasticsearch__.delete_index!
-Message.__elasticsearch__.create_index! force: true
-Message.__elasticsearch__.refresh_index!
-Message.import(force: true)
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,23 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /instabug/tmp/pids/server.pid
+
+echo "Running rabbitmq healthcheck..."
+echo "Waiting for rabbitmq to get healthy..."
+timeout 300 bash -c "until curl --silent --output /dev/null http://$RABBITMQ_HOST:15672/; do printf '.'; sleep 5; done; printf '\n'"
+
+echo "Running elasticsearch healthcheck..."
+echo "Waiting for elasticsearch to get healthy..."
+timeout 300 bash -c "until curl --silent --output /dev/null http://$ELASTICSEARCH_HOST:9200/_cat/health?h=st; do printf '.'; sleep 5; done; printf '\n'"
+
+if [ -z $WORKER ]
+then
+    rake db:create;
+    rake db:test:prepare;
+    rails db:migrate;
+    rails db:seed;
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
